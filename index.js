8f701c579c8d6ce32cc45d48f693769aa64bd237
Ivy <veesjam@gmail.com>
2025-03-08 04:52:35 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// select image element
+const img = document.getElementById('images');
+// listen for the click event
+img.addEventListener('click', function(){
+
+    img.src = 'media\\explosion.gif';
+
+})
